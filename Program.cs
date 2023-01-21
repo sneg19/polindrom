44145bdc934741daaa49c5fa9c463f2d36926dfc
sneg <sneg.sbg@gmail.com>
2023-01-21 16:21:18 +0300

    working programm

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine()!);
+// 12345
+int num1 = number % 10;
+int num2 = number / 10000;
+int num3 = number / 100 % 10;
+int num4 = number / 1000 % 10;
+
+if (num1 == num2 || num3 == num4) Console.WriteLine($"Число {number} является полиндромом");
+else Console.WriteLine($"Число {number} не является полиндромом");
